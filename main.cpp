3d96bfcd478969adb74f9d8359935254c5ec2b86
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-23 22:07:48 +0300

    second commit: add 2nd task - binary read from file

--- main.cpp
+++ main.cpp
@@ -3,19 +3,43 @@
 
 int main() {
     //19.5.1 search and count exact word in text
-    std::fstream text("C:\\Users\\Anton\\Desktop\\SecondStart C++\\lesson 19 ifstream\\HW\\words.txt");
-    std::string word;
-    int count = 0;
-    std::cout << "Input word for search: ";
-    std::cin >> word;
-    for(std::string buffer; !text.eof(); ) {
-        text >> buffer;
-        if (buffer == word || buffer == word + '.' || buffer == word + ',')
-            count++;
+    /*
+    std::fstream text("words.txt");
+    if(text.is_open()) {
+        std::string word;
+        int count = 0;
+        std::cout << "Input word for search: ";
+        std::cin >> word;
+        for(std::string buffer; !text.eof(); ) {
+            text >> buffer;
+            if (buffer == word || buffer == word + '.' || buffer == word + ',')
+                count++;
+        }
+        std::cout << "There are " << count << " word: " << word << " in words.txt" << std::endl;
+    } else {
+        std::cout << "Incorrect file path!" << std::endl;
     }
-    std::cout << "There are " << count << " word: " << word << " in words.txt" << std::endl;
-
-    text.close();
     
+    text.close();
+    */
+    
+    // 19.5.2 binary reading from file
+    std::ifstream text;
+    text.open("words.txt", std::ios::binary);
+    
+    if (text.is_open()) {
+        for (char buff[20]; !text.eof(); ) {
+            int end = sizeof(buff) - 1;
+            text.read(buff, end);
+            if (text.gcount() < end) 
+                end = text.gcount();
+            buff[end] = 0;
+            std::cout << buff;
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Incorrect file path!" << std::endl;
+    }
+    text.close();
 
 }
