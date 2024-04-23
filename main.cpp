1c617550b35efc8bc037f3f1dd5825c734efea73
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-23 21:33:13 +0300

    first commit: solve 1 task - read from file and count exact words

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
+    //19.5.1 search and count exact word in text
+    std::fstream text("C:\\Users\\Anton\\Desktop\\SecondStart C++\\lesson 19 ifstream\\HW\\words.txt");
+    std::string word;
+    int count = 0;
+    std::cout << "Input word for search: ";
+    std::cin >> word;
+    for(std::string buffer; !text.eof(); ) {
+        text >> buffer;
+        if (buffer == word || buffer == word + '.' || buffer == word + ',')
+            count++;
+    }
+    std::cout << "There are " << count << " word: " << word << " in words.txt" << std::endl;
+
+    text.close();
     
+
 }
